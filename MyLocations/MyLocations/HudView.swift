58700de2d937d5d4f0c25ecab5ee3bc221f65bde
JovannyEspinal <espinaljovanny@icloud.com>
2015-12-26 19:02:47 -0500

    Added spring animation to HUD view on Tag Location screen

--- MyLocations/MyLocations/HudView.swift
+++ MyLocations/MyLocations/HudView.swift
@@ -19,7 +19,7 @@ class HudView: UIView {
         view.userInteractionEnabled = false
 //        
 //        hudView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
-        
+        hudView.showAnimated(animated)
         return hudView
     }
     
@@ -46,5 +46,16 @@ class HudView: UIView {
         let textPoint = CGPoint(x: center.x - round(textSize.width / 2), y: center.y - round(textSize.height / 2) + boxHeight / 4)
         
         text.drawAtPoint(textPoint, withAttributes: attribs)
+    }
+    
+    func showAnimated(animated: Bool) {
+        if animated {
+            alpha = 0
+            transform = CGAffineTransformMakeScale(1.3, 1.3)
+            UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: [], animations: {
+                self.alpha = 1
+                self.transform = CGAffineTransformIdentity
+                }, completion: nil)
+        }
     }
 }
